ae65680aaa59ae9ae05862069c63f03400d1fca1
Cry7ek <iliafilipp1nov@gmail.com>
2025-05-07 18:57:37 +0300

    SuperMenuCommit

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -7,26 +7,29 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] Button _StartButton;
     [SerializeField] Button _AchievementButton;
+    [SerializeField] Button _ExitInAchievementButton;
     [SerializeField] Button _ExitButton;
     [SerializeField] GameObject _AchievementPanel;
     [SerializeField] GameObject _MainMenuPanel;
-    public void AfterClickAchievement()
-    {
-
-        _AchievementPanel.SetActive(!_AchievementPanel.activeInHierarchy);
-        _MainMenuPanel.SetActive(!_MainMenuPanel.activeInHierarchy);
-    }
 
     private void Awake()
     {
+        _ExitInAchievementButton.onClick.AddListener(AfterClickAchievement);
         _AchievementButton.onClick.AddListener(AfterClickAchievement);
         _StartButton.onClick.AddListener(AfterClickStart);
         _ExitButton.onClick.AddListener(AfterClickExit);
     }
+    public void AfterClickAchievement()
+    {
+        _AchievementPanel.SetActive(!_AchievementPanel.activeInHierarchy);
+        _MainMenuPanel.SetActive(!_MainMenuPanel.activeInHierarchy);
+    }
+
     public void AfterClickStart()
     {
         SceneManager.LoadScene("LoadingScene");
     }
+
     public void AfterClickExit()
     {
             UnityEditor.EditorApplication.isPlaying = false;
