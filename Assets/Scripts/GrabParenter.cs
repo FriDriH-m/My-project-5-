5fa0339922594945168346b73a478cca4b9e3bc4
Musa Musaev <good.24165@gmail.com>
2025-05-12 18:43:32 +0300

    Commit

--- Assets/Scripts/GrabParenter.cs
+++ Assets/Scripts/GrabParenter.cs
@@ -16,6 +16,7 @@ public class GrabParenter : MonoBehaviour
     [SerializeField] private Vector3 _rightHandPosition; //
     [SerializeField] private Vector3 _rightHandEuler;    //
     [SerializeField] private Vector3 _twoHandEuler; // вращение в двуручном хвате
+    [SerializeField] private float _trackPoint = 0.5f;
     private Quaternion _leftHandRotation => Quaternion.Euler(_leftHandEuler); // преобразование Vector3 в Quaternion и запись в переменную
     private Quaternion _rightHandRotation => Quaternion.Euler(_rightHandEuler); // преобразование Vector3 в Quaternion и запись в переменную
     private Quaternion _twoHandRotation => Quaternion.Euler(_twoHandEuler); // преобразование Vector3 в Quaternion и запись в переменную
@@ -42,12 +43,21 @@ public class GrabParenter : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         rb.centerOfMass = _offset;
     }
+    private void Start()
+    {
+        if (!_canHoldTwoHands)
+        {
+            grabInteractable.selectMode = InteractableSelectMode.Single;
+            grabInteractable.focusMode = InteractableFocusMode.Single;
+        }
+    }
+
     public void OnGrab(SelectEnterEventArgs args)
     {
         interactable = args.interactableObject.transform;
         interactor = args.interactorObject.transform;
         // Начало твоего скрипта, Илья
-        if (item.inSlot && item != null)
+        if (item != null && item.inSlot)
         {
             _audioSource.PlayOneShot(_selectItem);
             currentSlot = item.currentSlot;
@@ -61,28 +71,6 @@ public class GrabParenter : MonoBehaviour
                 _firstHand = interactor;
                 interactable.SetParent(interactor); // задается родитель в виде Near-Far Interactore в соответсвующем контроллере
                 HandToTargetPosition(_firstHand);   // метод задает позицию для рук
-            }
-            if (_canHoldTwoHands) // если оружие можно держать двумя руками (одноручное нельзя будет). Указывается в инспекторе
-            {
-                if (interactor.transform != _firstHand) // если берешь той рукой, которой до этого не брал. _firstHand принимает значение при первом взятии, всегда.
-                {
-                    _secondaryHand = interactor;
-                    if (_firstHand.CompareTag("R_Hand")) // вычисляется тег второй руки. Если у _firstHand тег R_Hand, то у второго L_Hand :O (ибануться можно).
-                    {
-                        if (_leftHand.GetComponent<Collider>() != null) { _leftHand.GetComponent<Collider>().enabled = false; } // отключает коллайдер чтобы не мешало
-                        _leftHand.transform.SetParent(interactable); // родителем модельки руки становится меч
-                        _leftHand.transform.localPosition = _leftHandPosition - new Vector3(0, 0.2f, 0); // просто позиционирование
-                        _leftHand.transform.localRotation = _leftHandRotation;
-                    }
-                    else // начинка та же
-                    {
-                        if (_rightHand.GetComponent<Collider>() != null) { _rightHand.GetComponent<Collider>().enabled = false; }
-                        _rightHand.transform.SetParent(interactable);
-                        _rightHand.transform.localPosition = _rightHandPosition - new Vector3(0, 0.2f, 0);
-                        _rightHand.transform.localRotation = _rightHandRotation;
-                    }
-                    TwoHandGrab(false);
-                }
             }
             // конец части моего скрипта
 
@@ -107,6 +95,7 @@ public class GrabParenter : MonoBehaviour
             _firstHand = interactor;
             interactable.SetParent(interactor); // задается родитель в виде Near-Far Interactore в соответсвующем контроллере
             HandToTargetPosition(_firstHand);
+            SetRigidbodyDumping(70f);
         }
         if (_canHoldTwoHands)
         {
@@ -128,10 +117,9 @@ public class GrabParenter : MonoBehaviour
                     _rightHand.transform.localRotation = _rightHandRotation;
                 }
                 TwoHandGrab(false);
+                SetRigidbodyDumping(40f);
             }
         }   
-        //rb.linearDamping = 70f;
-        //rb.angularDamping = 70f;
     }
 
     public void OnUngrab(SelectExitEventArgs args)
@@ -147,11 +135,11 @@ public class GrabParenter : MonoBehaviour
 
         if (_firstHand != null && _secondaryHand != null) // если при отпускании оружие взято обеими руками
         {
-            if (args.interactorObject.transform == _firstHand) // если отпускает первая рука, которая взялась за оружие
+            if (interactor == _firstHand) // если отпускает первая рука, которая взялась за оружие
             {
                 HandToStartPosition(_firstHand); // задается начальная позиция, то есть на Родину депортируют :,(
                 _firstHand = _secondaryHand; // _firstHand принимает значение _secondaryHand чтобы в дальнейшем если отпущенной рукой возьмется игрок не было базара
-                interactable.SetParent(interactor); // моделька руки становится дочерним объектом контроллера
+                interactable.SetParent(_secondaryHand); // моделька руки становится дочерним объектом контроллера
                 _secondaryHand = null;
                 TwoHandGrab(true); // включается трекинг позиции, так как для одной руки он нужен чтобы оружие следовало за контроллером и крутилось. При двуручном мы отключаем это чтобы самим крутить-вертеть
             }
@@ -161,6 +149,7 @@ public class GrabParenter : MonoBehaviour
                 TwoHandGrab(true);
                 _secondaryHand = null;
             }
+            SetRigidbodyDumping(70f);
         }
         else // это обрабатывается если двуручного хвата не было, то есть одной рукой взял и этой же рукой отпустил
         {
@@ -168,26 +157,32 @@ public class GrabParenter : MonoBehaviour
             HandToStartPosition(_firstHand);            
             _firstHand = null;
             interactable.SetParent(null);
+            SetRigidbodyDumping(0f);
         }        
     }
     public void TwoHandGrab(bool value)
     {
         grabInteractable.trackPosition = value;
-        grabInteractable.trackRotation = value;
+        //grabInteractable.trackRotation = value;
+    }
+    public void SetRigidbodyDumping(float value)
+    {
+        rb.angularDamping = value;
+        rb.linearDamping = value;
     }
     public void HandToStartPosition(Transform hand)
     {
         if (hand.CompareTag("L_Hand"))
         {
             if (_leftHand.GetComponent<Collider>() != null) { _leftHand.GetComponent<Collider>().enabled = true; }            
-            _leftHand.transform.SetParent(interactor);
+            _leftHand.transform.SetParent(hand);
             _leftHand.transform.localPosition = Vector3.zero;
             _leftHand.transform.localRotation = Quaternion.Euler(-180, 167.888f, -90);
         }
         else if (hand.CompareTag("R_Hand"))
         {
             if (_rightHand.GetComponent<Collider>() != null) { _rightHand.GetComponent<Collider>().enabled = true; }
-            _rightHand.transform.SetParent(interactor);
+            _rightHand.transform.SetParent(hand);
             _rightHand.transform.localPosition = Vector3.zero;
             _rightHand.transform.localRotation = Quaternion.Euler(-180, 187.945f, 90);
         }
@@ -213,11 +208,13 @@ public class GrabParenter : MonoBehaviour
     {
         if (_firstHand != null && _secondaryHand != null)
         {
-            Vector3 midPoint = Vector3.Lerp(_firstHand.position, _secondaryHand.position, 0.5f);
-            transform.position = midPoint;
-
+            
+            Vector3 midPoint = Vector3.Lerp(_firstHand.position, _secondaryHand.position, _trackPoint);
             Vector3 direction = _firstHand.position - _secondaryHand.position;
-            transform.rotation = Quaternion.LookRotation(direction) * _twoHandRotation;
+
+            // Используй физические методы для перемещения
+            rb.AddForce((midPoint - transform.position) * 10, ForceMode.VelocityChange);
+            //rb.AddTorque((direction - transform.eulerAngles) , ForceMode.VelocityChange);
         }
     }
     private IEnumerator EnableCollision(float duration)
