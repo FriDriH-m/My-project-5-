00c60ad84bd9e63de756b943b5fe2a36132a8cdb
Musa Musaev <good.24165@gmail.com>
2025-05-23 13:34:42 +0300

    Смерть от падения 2

--- Assets/Scripts/HoleScript.cs
+++ Assets/Scripts/HoleScript.cs
@@ -4,18 +4,39 @@ public class HoleScript : MonoBehaviour
 {
     [SerializeField] private Vector3 respawnPoint;
     [SerializeField] private Transform player;
+    [SerializeField] private ParticleSystem _effect;
+    private Transform _parent;
     private void OnTriggerEnter(Collider other)
     {        
         Debug.Log("Something");
         if (other.transform.CompareTag("Weapon"))
         {
-            Debug.Log("Weapon");
-            other.transform.position = player.position + new Vector3(0.3f, 0.3f, 0.3f);
+            FindParent(other);
+            _parent.transform.position = player.position + new Vector3(2, 3, 0);
+            _parent.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+            _parent.rotation = Quaternion.identity;
+            Instantiate(_effect, _parent.position, Quaternion.identity);
         }
         if (other.GetComponentInChildren<PlayerDamage>() != null)
         {
             Debug.Log("player");
             other.GetComponent<PlayerDamage>().hitPoints = 0;
+        }
+    }
+
+    private void FindParent(Collider other)
+    {
+        for (int i = 0; i < other.transform.childCount; i++)
+        {
+            _parent = other.transform.parent;
+            if (_parent == null)
+            {
+                return;
+            }
+            else
+            {
+                _parent = _parent.parent;
+            }
         }
     }
 }
