6c00cfa5ea489571fd2731c7a36fc9214dc7bd81
Cry7ek <iliafilipp1nov@gmail.com>
2025-05-28 17:57:08 +0300

    GoldBush
    Подорожники+ачивки

--- Assets/Scripts/Damage/Healing.cs
+++ Assets/Scripts/Damage/Healing.cs
@@ -5,6 +5,7 @@ public class Healing : MonoBehaviour
 {
     [SerializeField] public Collider _podorozhniktrigger; //Триггер, который чекает вход подорожника (На MainCamera)
     [SerializeField] private int _healAmount = 30; // Насколько хиллимся
+    public DataAchievement Icon7;
 
     private PlayerDamage _playerDamage;
     public Image Bar;
@@ -36,6 +37,11 @@ public class Healing : MonoBehaviour
     private void HealthBar()
     {
         Bar.fillAmount = _playerDamage.hitPoints / 200;
+    }
+
+    public void OnGrabAchiv()
+    {
+        Icon7._unlocked = true;
     }
 
     private void DisableBush() // Минусует подорожник, чтоб больше не возникал
