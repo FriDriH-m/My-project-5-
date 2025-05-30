509241c5423d08b3518d7d6f1ff7fcec4e8a2e92
Cry7ek <iliafilipp1nov@gmail.com>
2025-05-30 18:29:36 +0300

    недоделка

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class DoorTrigger : MonoBehaviour {
     [SerializeField] private GameObject door;
@@ -6,7 +7,9 @@ public class DoorTrigger : MonoBehaviour {
     private bool shouldMove = false;
     private Vector3 targetPosition;
     private bool wasMoved = false;
-
+    public AudioSource audioSource;
+    public AudioClip DoorOpenSound;
+    bool flag = false;
     public void Start() {
         targetPosition = door.transform.position + new Vector3(0f, -8f, 0f); 
     }
@@ -14,13 +17,25 @@ public class DoorTrigger : MonoBehaviour {
         if (PlitaTrigger.boxCount >= 5 && !wasMoved) { shouldMove = true; wasMoved = true; }
         else return;
     }
-    public void Update() {
+    public void Update() 
+    {
         if (shouldMove) {
             door.transform.position = Vector3.Lerp(door.transform.position, targetPosition, moveSpeed * Time.deltaTime);
         }
         if (Vector3.Distance(door.transform.position, targetPosition) < 0.5f)
         {
             shouldMove = false;
-        } 
+        }
+            if (shouldMove)
+            {
+                if (!flag)
+                {
+                    audioSource.Play();
+                flag = true;
+                }
+                
+            }
+        else
+            audioSource.Stop();
     }
 }
